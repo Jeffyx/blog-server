9cfc0b00df1d259a448aef9b5c5d4867ba29cb5a
xiejj <v-xiejj05@vanke.com>
2018-04-20 11:47:24 +0800

    增加跨域

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 const router = require('./router');
+const cors = require('koa2-cors');
 
+app.use(cors());
 app.use(bodyParser());
 
 router(app);
