2b5ba40ded3ef5a7e1bc085c217b32a96159c391
xiejj <v-xiejj05@vanke.com>
2018-05-15 09:56:59 +0800

    config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
 const config = {
   user: "postgres",
   host: "127.0.0.1",
-  database: "BLOG_CMS",
-  password: "hellojsung",
+  database: "blogdb",
+  password: "jeffy123.",
   port: 5432
 };
 // 数据库连接配置
